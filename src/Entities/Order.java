390f60e7284df2db02b4d2a3a4872b7d6649f61f
Cezar de sa <cezarsa9@gmail.com>
2023-01-08 23:07:50 -0300

    App finalizado

--- src/Entities/Order.java
+++ src/Entities/Order.java
@@ -1,5 +1,6 @@
 package Entities;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -7,15 +8,18 @@ import java.util.List;
 import Entities.enums.OrderStatus;
 
 public class Order {
+	
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
 	private Client client;
 	private Date moment;
 	private OrderStatus orderStatus;
 	private List<OrderItem> orderItems = new ArrayList<>();
 	
-	public Order(Client client, Date moment, OrderStatus orderStatus) {
-		this.client = client;
+	public Order( Date moment, OrderStatus orderStatus, Client client) {
 		this.moment = moment;
 		this.orderStatus = orderStatus;
+		this.client = client;				
 	}
 
 	public Order() {
@@ -51,6 +55,32 @@ public class Order {
 
 	public void setOrderItems(List<OrderItem> orderItems) {
 		this.orderItems = orderItems;
+	}
+	
+	public double total() {
+		double sum = 0.0;
+		for (OrderItem item : orderItems) {
+			sum += item.subTotal();
+		}
+		return sum;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Order moment: ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append("Order status: ");
+		sb.append(orderStatus + "\n");
+		sb.append("Client: ");
+		sb.append(client + "\n");
+		sb.append("Order items:\n");
+		for (OrderItem order : orderItems) {
+			sb.append(order + "\n");
+		}
+		sb.append("Total price: $");
+		sb.append(String.format("%.2f", total()));
+		return sb.toString();
 	}
 	
 	
--- src/Entities/OrderItem.java
+++ src/Entities/OrderItem.java
@@ -1,5 +1,43 @@
 package Entities;
 
 public class OrderItem {
+	private Product product;
+	private int quantity;
+	private double price;
+		
+	public OrderItem() {
+	}
 
+	public OrderItem(int quantity, double price, Product product) {
+		this.quantity = quantity;
+		this.price = price;
+		this.product = product;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public double subTotal() {
+		return quantity * price;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderItem [product=" + product + ", quantity=" + quantity + ", price=" + price + "]";
+	}
+	
+	
 }
